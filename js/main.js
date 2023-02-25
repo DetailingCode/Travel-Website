95291b3189eafb2c708d390bc2eb9d57a7e956c8
DetailingCode <doniek77@o2.pl>
2023-02-25 23:46:14 +0100

    Main -> Dream section finished

--- js/main.js
+++ js/main.js
@@ -5,13 +5,22 @@ const allNavItems = document.querySelectorAll(".nav__item");
 const handleNav = () => {
 	navActive.classList.toggle("nav--active");
 
-    allNavItems.forEach(item=> {
-        item.addEventListener('click', ()=> {
-            navActive.classList.remove('nav--active')
-        })
-    })
+	allNavItems.forEach((item) => {
+		item.addEventListener("click", () => {
+			navActive.classList.remove("nav--active");
+		});
+	});
+	handleNavItemAnimation();
 };
 
- 
+const handleNavItemAnimation = () => {
+	let delayTime = 0;
+
+	allNavItems.forEach(item => {
+		item.classList.toggle("nav-items-animation");
+		item.style.animationDelay = "." + delayTime + "s";
+		delayTime++;
+	});
+};
+
 navBtn.addEventListener("click", handleNav);
- 
